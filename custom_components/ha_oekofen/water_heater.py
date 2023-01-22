8189a01f36e8d4048059aefe9e2003dfad387595
Christian Karrié <christian@karrie.info>
2023-01-22 10:47:34 +0100

    debug

--- custom_components/ha_oekofen/water_heater.py
+++ custom_components/ha_oekofen/water_heater.py
@@ -36,30 +36,32 @@ class OekofenWaterHeater(OekofenCoordinatorEntity, WaterHeaterEntity):
     @property
     def current_temperature(self):
         """Return the current temperature."""
-        return self.coordinator.data.dhw.temperature
+        return self.coordinator.data.api.get_heating_circuit_temp()
 
     @property
     def current_operation(self):
         """Return current operation."""
-        operation = self.coordinator.data.dhw.current_operation
+        operation = self.coordinator.data.api.get_heating_circuit_state()
         return operation if operation in self.operation_list else STATE_OFF
 
     async def async_set_temperature(self, **kwargs: Any) -> None:
         """Set new target temperature."""
-        if await self.coordinator.data.dhw.set_temp(kwargs.get(ATTR_TEMPERATURE)):
+        print("[water_heater.OekofenWaterHeater.async_set_temperature] with kwags %s" % kwargs)
+        if await self.coordinator.data.api.set_heating_circuit_temp(celsius=kwargs.get(ATTR_TEMPERATURE)):
             self.async_write_ha_state()
 
     @property
     def target_temperature(self):
         """Return the setpoint if water demand, otherwise return base temp (comfort level)."""
-        return self.coordinator.data.dhw.target_temperature
+        attr = self.coordinator.data.api.get_attribute('hk', 'L_flowtemp_set')
+        return attr.get_value()
 
     @property
     def max_temp(self) -> float:
         """Return the maximum temperature."""
-        return self.coordinator.data.dhw.max_temp
+        return 40.0
 
     @property
     def min_temp(self) -> float:
         """Return the minimum temperature."""
-        return self.coordinator.data.dhw.min_temp
+        return 15.0
