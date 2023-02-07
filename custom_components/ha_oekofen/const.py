7a2ee32a211e35d8f380a7d09b543181d51752ed
Konrad <konrad@labk.it>
2023-02-07 11:09:22 +0100

    removed duplicated L_pump

--- custom_components/ha_oekofen/const.py
+++ custom_components/ha_oekofen/const.py
@@ -87,8 +87,7 @@ TIME_SENSORS_BY_DOMAIN = {
 
 # Unit: %
 NON_PUMP_PERCENTAGE_SENSORS_BY_DOMAIN = {
-    'pe': ['L_currentairflow', 'L_fluegas', 'L_uw_speed', 'L_uw', 'L_modulation'],
-    'sk': ['L_pump'],
+    'pe': ['L_currentairflow', 'L_fluegas', 'L_uw_speed', 'L_uw', 'L_modulation']
 }
 
 NON_PUMP_BINARY_SENSORS_BY_DOMAIN = {
